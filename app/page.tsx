979d9d579de5b2ca8b31fc662ff13d2006b6b924
Jonathan Hazan <yonatanhazan1337@gmail.com>
2024-08-05 11:09:23 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,17 @@ export default function Home() {
   return (
     <div className="max-w-7xl mx-auto px-4 md:px-8 mt-24">
         <div className="max-w-3xl mx-auto text-2xl sm:text-5xl lg:6xl font-semibold text-center">
-          <h1>Find the best Anime merch</h1>
-          <h1 className="text-primary text-1xl sm:text-4xl lg:5xl">Clothes, figures & mangas</h1>
+          <h1>Find the best <strong>Anime</strong> marketplace</h1>
+          <h1 className="text-primary text-1xl sm:text-4xl lg:5xl">Sell and Buy clothes, figures & mangas</h1>
           <p className="lg:text-lg text-muted-foreground mx-auto mt-5 w-[90%] font-normal text-base">This is the best place to find the best Anime merch for your next adventure</p>
         </div>
         <ProductRow category="newest" />
         <ProductRow category="clothes" />
         <ProductRow category="figures" />
-        <ProductRow category="manga" />
+        <div className="mb-24">
+
+        <ProductRow category="manga"/>
+        </div>
     </div>
   );
 }
